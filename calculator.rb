698944f398eecbd7f73f39e239124a1ee9e31851
Joseph Miller <jdeshaiseworldtv@gmail.com>
2019-09-12 03:39:52 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,14 +1,5 @@
-./calculator.rb
-  contains a local variable called first_number that is assigned to a number (FAILED - 1)
-
-Failures:
-
-  1) ./calculator.rb contains a local variable called first_number that is assigned to a number
-     Failure/Error: expect(first_number).to be_an(Integer).or be_a(Float)
-
-          expected "Hello world!" to be a kind of Integer
-
-       ...or:
-
-          expected "Hello world!" to be a kind of Float
-     # ./spec/calculator_spec.rb:8:in `block (2 levels) in <top (required)>'
+it "contains a local variable called first_number that is assigned to a number" do
+  first_number = get_variable_from_file('./calculator.rb', "first_number")
+ 
+  expect(first_number).to be_an(Integer).or be_a(Float)
+end
